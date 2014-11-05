d5b7323b4c66811dd0124c3365b4981c7dab9786
Imbris <danavain@mail.ru>
2014-11-05 22:49:51 +0300

    version 1.0 без перевода

---
+++ admin/main.php
@@ -0,0 +1,66 @@
+<?php
+
+add_action('admin_menu', 'callback_add_menu');
+
+//добавляем пункт меню с настройками
+function callback_add_menu() {
+        //появится в общих настройках
+        //add_options_page('CallBack', 'CallBack', 'manage_options', 'callback_options', 'callback_options_page'); 
+        
+        // добавляет пункт меню в WooCommerce
+        add_submenu_page('woocommerce', 'CallBack', 'Callback', 8, CALLBACK_LINK, 'callback_options_page');
+        
+        add_action( 'admin_init', 'register_wc_callback_settings' );
+        
+}
+
+function register_wc_callback_settings() {
+  register_setting( 'wc-callback' , 'wccb_popup_text' );
+  register_setting( 'wc-callback' , 'wccb_replace_cart' );
+  register_setting( 'wc-callback' , 'licence_key' );
+}
+
+//добавляем ссылку на страницу настроек со страницы плагинов
+
+add_filter( 'plugin_action_links', 'filter_callback_link', 10, 4 );
+
+function filter_callback_link ($links, $file) {
+    if ( $file != WPWCCB_PLUGIN_DIR) return $links;
+    
+    //$settings_link = '<a href="admin.php?page=$options_link">' . __('Settings') . '</a>';
+    $link = '<a href="admin.php?page=' . CALLBACK_LINK . '">' . __('Settings') . '</a>';
+    array_unshift($links, $link);
+    return $links;
+}
+
+//страница настроек
+function callback_options_page() {
+    ?>
+    <div class="wrap">
+    <h2><?php _e('Settings') ?> WooCommerce CallBack</h2>
+    <form method="post" action="options.php">
+    <?php settings_fields( 'wc-callback' ); ?>
+    <table class="form-table">
+        <tr valign="top">
+            <th scope="row"><?php _e('Popup window contents','woocommerce_callback') ?></th>
+             <td>
+                 <textarea name="wccb_popup_text" rows="5" cols="50"><?php echo get_option('wccb_popup_text'); ?></textarea>
+                 <p class="description"><?php _e('Insert form shortcode here','woocommerce_callback') ?></p>
+             </td>
+        </tr>
+        <tr>
+             <th scope="row"><?php _e('Replace "Proceed to Checkout" button in the cart','woocommerce_callback') ?></th>
+             <td>
+                 <input type="checkbox" name="wccb_replace_cart" value="1" <?php if(get_option('wccb_replace_cart')):?>checked="checked"<?php endif;?> />
+             </td>
+        </tr>
+    </table>
+    <p class="submit">
+    <input type="submit" class="button-primary" value="<?php _e('Save Changes') ?>" />
+    </p>
+    </form>
+    </div>
+    <?php
+}
+
+?>
---
+++ woocommerce_callback.php
@@ -0,0 +1,82 @@
+<?php
+/*
+Plugin Name: WooCommerce Callback Button
+Plugin URI: 
+Description: Добавляет кнопку обратного звонка к магазину WooCommerce. Для корректной работы всплывающего окна необходим плагин Easy FancyBox
+Version: 1.0
+Author: Imbris
+Author URI: 
+*/
+
+/*  Copyright 2014  Imbris  (email: d.p.vainstein@gmail.com)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/ 
+
+
+//Задачи!
+//либо встроить использование fancybox внутрь плагина, либо использовать стандартные возможности woocommerce и callisto
+//добавить замену кнопки корзины
+
+define(WPWCCB_PLUGIN_DIR, plugin_basename(__FILE__));
+define ( CALLBACK_LINK, 'callback_options');
+
+//загружаем настройки
+
+//опции добавляются через register_setting
+//$option_value = 'что-то написано!';
+//add_option('wccb_popup_text', $option_value);
+
+
+//подключаем перевод
+add_action('init', 'woocommerce_callback_translate');
+ 
+function woocommerce_callback_translate() {
+          load_plugin_textdomain('woocommerce_callback', false, dirname( plugin_basename( __FILE__ ) ) . '/languages/');
+}
+do_action('woocommerce_callback_translate');
+
+//подключаем стили
+function callback_styles()
+{
+    // Register the style like this for a plugin:
+    wp_register_style( 'callback_style', plugins_url( '/style.css', __FILE__ ));
+
+    // For either a plugin or a theme, you can then enqueue the style:
+    wp_enqueue_style( 'callback_style' );
+}
+add_action( 'wp_enqueue_scripts', 'callback_styles', 99 );
+
+function callback_button() {
+    ?>
+    <a style="line-height: 19px;" class="fancybox button-3 call_back trans-1 custom-font-1" href="#contact_form_pop"><span style="background: none; padding: 5px 15px;"><?php _e('Сallback', 'woocommerce_callback'); ?></span></a>
+            <div style="display:none" class="fancybox-hidden">
+             <div id="contact_form_pop">     
+                <?php echo do_shortcode(get_option('wccb_popup_text')); ?>           
+                <?php //echo do_shortcode('[contact-form-7 id="2345" title="Обратный звонок"]');?>
+             </div>
+           </div>
+           <br clear="all"/>
+    <?php
+    }
+
+    //регистрируем hook для кнопки на странице товара
+    add_action('woocommerce_before_add_to_cart_form','callback_button');
+
+    //применяем hook
+    do_action('callback_button');
+    
+    include_once('admin/main.php');
+?>
